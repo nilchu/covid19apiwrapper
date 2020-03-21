488e9d1f9bdba1b2a83ddec1a5a1f3460c449a79
Nils Schuberth <nils.schuberth@gmail.com>
2020-03-21 19:06:58 +0100

    Add method skeleton

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+class Covid19Api {
+   
+    static baseURL: string = 'https://api.covid19api.com';
+    
+    /**
+     * Methods
+     */
+    static async getCountries() {
+
+    }
+
+    static async getSummery() {
+
+    }
+
+    static async getAll() {
+
+    }
+
+    static async getCasesForCountryWithStatus( params: object ) {
+
+    }
+
+}
