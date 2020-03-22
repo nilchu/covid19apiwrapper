709308cb9c67b90991b45eb64e837d99a862e945
nilchu <>
2020-03-22 02:33:31 +0100

    Add more methods to aggregate data

--- src/classes/Covid19Api.ts
+++ src/classes/Covid19Api.ts
@@ -22,12 +22,34 @@ export default class Covid19Api {
     private static baseUrl: string = 'https://api.covid19api.com';
     private static lastApiCallTimestamp: Number;
     private static cache: object = {
-
+        
     }
+    private static _cachingDuration: number;
+    private static _cachingActive: boolean;
     private static countryArray: Array<Country>;
     
     constructor( options?: object ) {
         Covid19Api.initCountryArray();
+    }
+
+    /**
+     * setter & getter
+     */
+
+    get cachingDuration() {
+        return Covid19Api._cachingDuration;
+    }
+
+    set cachingDuration(duration: number) {
+        Covid19Api._cachingDuration = duration;
+    }
+
+    get cachingActive() {
+        return Covid19Api._cachingActive;
+    }
+
+    set cachingActive(value: boolean) {
+        Covid19Api._cachingActive = value;
     }
 
     /**
@@ -62,7 +84,7 @@ export default class Covid19Api {
         }
     }
 
-    public static async getSummery() : Promise<object | undefined> {
+    public static async getSummary() : Promise<any | object | undefined> {
         try {
             return await axios.get(this.baseUrl + '/summary');
         } catch (error) {
@@ -113,5 +135,25 @@ export default class Covid19Api {
             
         }
     }
+
+    /**
+     * custom methods
+     */
+
+     public static async getNewCasesWorldwide() : Promise<number> {
+        const response = await this.getSummary();
+        const countryStats: Array<any> = response.data.Countries;
+        const numberNewCases: any = countryStats.reduce((prev: any, curr: any) => { return { NewConfirmed: prev.NewConfirmed + curr.NewConfirmed }});
+        
+        return numberNewCases.NewConfirmed;
+     } 
+
+     public static async getTotalCasesWorldwide(): Promise<number> {
+         const response = await this.getSummary();
+         const countryStats: Array<any> = response.data.Countries;
+         const numberTotalCases: any = countryStats.reduce((prev: any, curr: any) => { return { TotalConfirmed: prev.TotalConfirmed + curr.TotalConfirmed }});
+
+         return numberTotalCases.TotalConfirmed;
+     }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import Covid19Api from './classes/Covid19Api';
 
 export default Covid19Api;
+
+Covid19Api.getSummary()
+    .then((data) => {
+        //console.log(data.data)
+    })
+    .catch((e) => {
+        //console.log(e.message)
+    })
+
+Covid19Api.getTotalCasesWorldwide()
+    .then((number) => console.log(number))
+    .catch(e => console.log(e.message) )
